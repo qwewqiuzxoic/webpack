43e6d2d57e126c8307fde1a291ecb01096c73324
qwewqiuzxoic <eewt120@naver.com>
2019-06-22 11:21:16 +0900

    second message

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//commonJS 스팩의 모듈 가저오기 방식
+var _ = require('lodash');
+
+
+function component() {
+    let element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
